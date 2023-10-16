f420dff54bcae67eb1a00d37b19f3ae7752d6259
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-10-15 19:49:05 -0600

    Cadena de conexión

---
+++ Registro/Models/RegistroContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Registro.Models
+{
+    public class RegistroContext : DbContext
+    {
+        public DbSet<Estudiante> Estudiante { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=jhonson;Database=PrograII;Trusted_Connection=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=true;");
+        }
+    }
+}
