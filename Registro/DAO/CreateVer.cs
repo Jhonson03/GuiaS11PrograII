58746d0584dd9a989b52b8b7d21ce3c3cc28849a
Jhonson03 <123599062+Jhonson03@users.noreply.github.com>
2023-10-15 19:48:31 -0600

    Metodos para agregar y ver

---
+++ Registro/DAO/CreateVer.cs
@@ -0,0 +1,32 @@
+﻿using Registro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro.DAO
+{
+    public class CreateVer
+    {
+        RegistroContext db = new RegistroContext();
+
+        public void CreateEstu(Estudiante Es)
+        {
+                Estudiante Estu = new Estudiante();
+
+                Estu.Nombres = Es.Nombres;
+                Estu.Apellidos = Es.Apellidos;
+                Estu.Edad = Es.Edad;
+                Estu.Sexo = Es.Sexo;
+
+                db.Add(Estu);
+                db.SaveChanges();
+        }
+
+        public List<Estudiante> ListaEstudiante()
+        {
+            return db.Estudiante.ToList();
+        }
+    }
+}
